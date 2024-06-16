8b1c79f0fb86b44c82466485083c6313256661f2
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2024-06-16 21:36:16 +0900

    lombokの@Dataを使う

--- madoi-server/src/main/java/edu/kcg/futurelab/madoi/server/ApplicationProperties.java
+++ madoi-server/src/main/java/edu/kcg/futurelab/madoi/server/ApplicationProperties.java
@@ -3,21 +3,12 @@ package edu.kcg.futurelab.madoi.server;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import lombok.Data;
+
 @Component
 @ConfigurationProperties("madoi")
+@Data
 public class ApplicationProperties {
-	public String[] getAllowedOrigins() {
-		return allowedOrigins;
-	}
-	public void setAllowedOrigins(String[] allowedOrigins) {
-		this.allowedOrigins = allowedOrigins;
-	}
-	public String[] getApiKeys() {
-		return apiKeys;
-	}
-	public void setApiKeys(String[] apiKeys) {
-		this.apiKeys = apiKeys;
-	}
 	private String[] apiKeys;
 	private String[] allowedOrigins;
 }
