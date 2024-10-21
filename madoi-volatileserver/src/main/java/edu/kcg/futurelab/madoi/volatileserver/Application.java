c4a04200c60de9b9bfd7e7c1a6998dc23e396ac2
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2024-10-21 15:42:15 +0900

    不要なimportを削除

--- madoi-volatileserver/src/main/java/edu/kcg/futurelab/madoi/volatileserver/Application.java
+++ madoi-volatileserver/src/main/java/edu/kcg/futurelab/madoi/volatileserver/Application.java
@@ -3,9 +3,6 @@ package edu.kcg.futurelab.madoi.volatileserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @ServletComponentScan
