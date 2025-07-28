c709d8d3374033fbf0e2ae89c6b69b05b85aef91
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2025-07-29 00:04:49 +0900

    ビルドスクリプト更新

--- madoi-volatileserver/rebuild.sh
+++ madoi-volatileserver/rebuild.sh
@@ -5,11 +5,11 @@ mvn clean
 mvn install -DskipTests=true
 popd
 
-pushd ../madoi-client-ts-js
-npm ci
+pushd ../../madoi-client-ts-js
+npm i
 npm run build
 popd
-cp ../madoi-client-ts-js/dist/madoi.js ./webapp/js/
-cp ../madoi-client-ts-js/dist/madoi.d.ts ./webapp/js/
+cp ../../madoi-client-ts-js/dist/madoi.umd.js ./webapp/js/madoi.js
+cp ../../madoi-client-ts-js/types/madoi.d.ts ./webapp/js/
 
 mvn clean package -DskipTests=true
