e02033e280a37ebdd5765158438d80c3ddacafe5
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2025-09-18 23:54:22 +0900

    remove unused elements.

--- madoi-core/src/main/java/edu/kcg/futurelab/madoi/core/message/config/ShareConfig.java
+++ madoi-core/src/main/java/edu/kcg/futurelab/madoi/core/message/config/ShareConfig.java
@@ -7,15 +7,8 @@ public class ShareConfig{
 
 	public ShareConfig() {
 	}
-	public ShareConfig(SharingType type, int maxLog, String[] allowedTo, UpdateConfig update) {
+	public ShareConfig(SharingType type) {
 		this.type = type;
-		this.maxLog = maxLog;
-		this.allowedTo = allowedTo;
-		this.update = update;
-	}
-	public ShareConfig(SharingType type, int maxLog) {
-		this.type = type;
-		this.maxLog = maxLog;
 	}
 	public SharingType getType() {
 		return type;
@@ -23,28 +16,7 @@ public class ShareConfig{
 	public void setType(SharingType type) {
 		this.type = type;
 	}
-	public int getMaxLog() {
-		return maxLog;
-	}
-	public void setMaxLog(int maxLog) {
-		this.maxLog = maxLog;
-	}
-	public String[] getAllowedTo() {
-		return allowedTo;
-	}
-	public void setAllowedTo(String[] allowedTo) {
-		this.allowedTo = allowedTo;
-	}
-	public UpdateConfig getUpdate() {
-		return update;
-	}
-	public void setUpdate(UpdateConfig update) {
-		this.update = update;
-	}
 
 
 	private SharingType type;
-	private int maxLog;
-	private String[] allowedTo;
-	private UpdateConfig update;
 }
